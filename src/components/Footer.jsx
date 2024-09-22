4c4ec5781e563e31366fbcb7ca0596e52d7948d5
Laxman <laxmangehlot0709@gmail.com>
2024-09-22 12:34:58 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+          {/* Left  Section */}
+          <div>
+             <img className='mb-5 w-40 ' src={assets.logo} alt="" />
+             <p className='w-full md:w-2/3 text-gray-600 leading-6'>We are dedicated to providing personalized, high-quality medical care to our patients. With a focus on compassion and expertise, we strive to ensure your health and well-being. </p>
+          </div>
+
+           {/* Center Section */}
+           <div>
+              <p className='text-xl font-medium mb-5'>COMPANY</p>
+              <ul className='flex flex-col gap-2 text-gray-600 '>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Privacy Policy</li>
+              </ul>
+           </div>
+
+            {/* Right Section  */}
+          <div>
+             <p className='text-xl font-medium mb-5 '>GET IN TOUCH</p>
+             <ul className='flex flex-col gap-2 text-gray-600'>
+                <a href='#'>+91-7878751109</a>
+                <a href='#'>laxmangehlot0709@gmail.com</a>
+             </ul>
+          </div>
+       </div>
+        {/* CopyRight Section */}
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2024@ Prescripto - All Right Reserved. </p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
